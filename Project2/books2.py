7bcd60c7ea45e5ebe9e9342e04ccbde72dae0388
Eric Roby <ericroby@Erics-MacBook-Pro.local>
2023-03-22 12:06:28 -0400

    init update

--- Project2/books2.py
+++ Project2/books2.py
@@ -1,185 +1,119 @@
 from typing import Optional
-from fastapi import FastAPI, HTTPException, Request, status, Form, Header
+
+from fastapi import FastAPI, Path, Query, HTTPException
 from pydantic import BaseModel, Field
-from uuid import UUID
-from starlette.responses import JSONResponse
-
-
-class NegativeNumberException(Exception):
-    def __init__(self, books_to_return):
-        self.books_to_return = books_to_return
-
+from starlette import status
 
 app = FastAPI()
 
 
-class Book(BaseModel):
-    id: UUID
-    title: str = Field(min_length=1)
-    author: str = Field(min_length=1, max_length=100)
-    description: Optional[str] = Field(title="Description of the book",
-                                       max_length=100,
-                                       min_length=1)
-    rating: int = Field(gt=-1, lt=101)
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+    def __init__(self, id, title, author, description, rating, published_date):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(title='id is not needed')
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=0, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
 
     class Config:
         schema_extra = {
-            "example": {
-                "id": "11f4c2ea-1340-41f4-89f7-2852347bb0d1",
-                "title": "Computer Science Pro",
-                "author": "Codingwithroby",
-                "description": "A very nice description of a book",
-                "rating": 75
+            'example': {
+                'title': 'A new book',
+                'author': 'codingwithroby',
+                'description': 'A new description of a book',
+                'rating': 5,
+                'published_date': 2029
             }
         }
 
 
-class BookNoRating(BaseModel):
-    id: UUID
-    title: str = Field(min_length=1)
-    author: str
-    description: Optional[str] = Field(
-        None, title="description of the Book",
-        max_length=100,
-        min_length=1
-    )
+BOOKS = [
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2030),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2030),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 2029),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2028),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2027),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 2026)
+]
 
 
-BOOKS = []
-
-
-@app.exception_handler(NegativeNumberException)
-async def negative_number_exception_handler(request: Request,
-                                            exception: NegativeNumberException):
-    return JSONResponse(
-        status_code=418,
-        content={"message": f"Hey, why do you want {exception.books_to_return} "
-                            f"books? You need to read more!"}
-    )
-
-
-@app.post("/books/login")
-async def book_login(username: str = Form(...), password: str = Form(...)):
-    return {"username": username, "password": password}
-
-
-@app.get("/header")
-async def read_header(random_header: Optional[str] = Header(None)):
-    return {"Random-Header": random_header}
-
-
-@app.get("/")
-async def read_all_books(books_to_return: Optional[int] = None):
-
-    if books_to_return and books_to_return < 0:
-        raise NegativeNumberException(books_to_return=books_to_return)
-
-    if len(BOOKS) < 1:
-        create_books_no_api()
-
-    if books_to_return and len(BOOKS) >= books_to_return > 0:
-        i = 1
-        new_books = []
-        while i <= books_to_return:
-            new_books.append(BOOKS[i - 1])
-            i += 1
-        return new_books
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def read_all_books():
     return BOOKS
 
 
-@app.get("/book/{book_id}")
-async def read_book(book_id: UUID):
-    for x in BOOKS:
-        if x.id == book_id:
-            return x
-    raise raise_item_cannot_be_found_exception()
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    raise HTTPException(status_code=404, detail='Item not found')
 
 
-@app.get("/book/rating/{book_id}", response_model=BookNoRating)
-async def read_book_no_rating(book_id: UUID):
-    for x in BOOKS:
-        if x.id == book_id:
-            return x
-    raise raise_item_cannot_be_found_exception()
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == book_rating:
+            books_to_return.append(book)
+    return books_to_return
 
 
-@app.post("/", status_code=status.HTTP_201_CREATED)
-async def create_book(book: Book):
-    BOOKS.append(book)
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def read_books_by_publish_date(published_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
+
+
+@app.post("/create-book", status_code=status.HTTP_201_CREATED)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.dict())
+    BOOKS.append(find_book_id(new_book))
+
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
 
 
-@app.put("/{book_id}")
-async def update_book(book_id: UUID, book: Book):
-    counter = 0
-
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            BOOKS[counter - 1] = book
-            return BOOKS[counter - 1]
-    raise raise_item_cannot_be_found_exception()
-
-
-@app.delete("/{book_id}")
-async def delete_book(book_id: UUID):
-    counter = 0
-
-    for x in BOOKS:
-        counter += 1
-        if x.id == book_id:
-            del BOOKS[counter - 1]
-            return f'ID:{book_id} deleted'
-    raise raise_item_cannot_be_found_exception()
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book(book: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
+            book_changed = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 
 
-def create_books_no_api():
-    book_1 = Book(id="71f4c2ea-1340-41f4-89f7-2852347bb0d1",
-                  title="Title 1",
-                  author="Author 1",
-                  description="Description 1",
-                  rating=60)
-    book_2 = Book(id="21f4c2ea-1340-41f4-89f7-2852347bb0d1",
-                  title="Title 2",
-                  author="Author 2",
-                  description="Description 2",
-                  rating=70)
-    book_3 = Book(id="31f4c2ea-1340-41f4-89f7-2852347bb0d1",
-                  title="Title 3",
-                  author="Author 3",
-                  description="Description 3",
-                  rating=80)
-    book_4 = Book(id="41f4c2ea-1340-41f4-89f7-2852347bb0d1",
-                  title="Title 4",
-                  author="Author 4",
-                  description="Description 4",
-                  rating=90)
-    BOOKS.append(book_1)
-    BOOKS.append(book_2)
-    BOOKS.append(book_3)
-    BOOKS.append(book_4)
-
-
-def raise_item_cannot_be_found_exception():
-    return HTTPException(status_code=404,
-                         detail="Book not found",
-                         headers={"X-Header_Error":
-                                  "Nothing to be seen at the UUID"})
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
