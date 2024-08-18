98b5e18499814ab20a89d5620495c5a65a891707
Vipin Kumar <vipinkumar@shivnadarfoundation.org>
2024-08-18 22:16:03 +0530

    Completed APIs with Validation. Project 2

---
+++ MyProject/Project2/books2.py
@@ -0,0 +1,130 @@
+from typing import Optional
+from fastapi import FastAPI, Path, Query, HTTPException
+from pydantic import BaseModel, Field
+from starlette import status
+
+app = FastAPI()
+
+
+class Book:
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+    published_date: int
+
+    def __init__(self, id, title, author, description, rating, published_date) -> None:
+        self.id = id
+        self.title = title
+        self.author = author
+        self.description = description
+        self.rating = rating
+        self.published_date = published_date
+
+
+class BookRequest(BaseModel):
+    id: Optional[int] = Field(description="ID is not needed on create", default=None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(gt=-1, lt=6)
+    published_date: int = Field(gt=1999, lt=2031)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "Author Name",
+                "description": "A new book description",
+                "rating": 5,
+                "published_date": 2024,
+            }
+        }
+    }
+
+
+BOOKS = [
+    Book(1, "Computer Scipece Pro", "codingwithroby", "A very nice book!", 5, 2010),
+    Book(2, "Be fast with FastAPI", "codingwithroby", "A great book!", 5, 2011),
+    Book(3, "Master Endpoints", "codingwithroby", "A awesome book!", 5, 2010),
+    Book(4, "HP1", "Author 1", "Book Description 1", 2, 2015),
+    Book(5, "HP2", "Author 2", "Book Description 2", 3, 2024),
+    Book(6, "HP3", "Author 3", "Book Description 3", 1, 2020),
+]
+
+
+@app.get("/books", status_code=status.HTTP_200_OK)
+async def read_all_books():
+    return BOOKS
+
+
+@app.get("/books/{book_id}", status_code=status.HTTP_200_OK)
+async def read_book(book_id: int = Path(gt=0)):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+
+    raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.get("/books/", status_code=status.HTTP_200_OK)
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating >= book_rating:
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.get("/books/publish/", status_code=status.HTTP_200_OK)
+async def get_books_by_publish_date(publish_date: int = Query(gt=1999, lt=2031)):
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == publish_date:
+            books_to_return.append(book)
+
+    return books_to_return
+
+
+@app.post("/create_book", status_code=status.HTTP_201_CREATED)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    BOOKS.append(findBookId(new_book))
+
+    return BOOKS
+
+
+@app.put("/books/update_book", status_code=status.HTTP_204_NO_CONTENT)
+async def update_book_content(book_request: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_request.id:
+            BOOKS[i] = book_request
+            book_changed = True
+
+    if book_changed == False:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+@app.delete("/books/{book_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_book(book_id: int = Path(gt=0)):
+    book_deleted = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_deleted = True
+            break
+
+    if book_deleted == False:
+        raise HTTPException(status_code=404, detail="Item not found")
+
+
+def findBookId(book: Book):
+    if len(BOOKS) > 0:
+        book.id = BOOKS[-1].id + 1
+    else:
+        book.id = 1
+
+    return book
--- notes.md
+++ notes.md
@@ -18,4 +18,19 @@ fastapi run main.py
 ```
 
 ## 2. Path Parameters:
-> Path parameters are request parameters that have been attached to a URL
+- Path parameters are request parameters that have been attached to a URL
+
+## 3. Pydantics:
+- Python library which is used for data validation/ data modeling/ data parsing/ error handling
+- How to handle data coming to out FastAPI application
+
+## 4. Status Codes:
+- An HTTP status code is used to help the Client (the user or system submitting data to the server) to understand what happened on the server side application.
+- International standards on how a Client/ Server should handle the result of request.
+- It allows everyone who sends a request if the submittion was successful of not.
+- Types:
+    - 1xx - Information Response: Request Processing.
+    - 2xx - Success: Requests successfully complete.
+    - 3xx - Redirection: Further action must be complete.
+    - 4xx - Client Errors: An error was caused by client.
+    - 5xx - Server Errors: An error occurred on the server.
